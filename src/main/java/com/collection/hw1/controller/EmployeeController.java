93eb15f07b8d58b654a416518f01dae36eed6164
Ильмира <kushkarova.1998@mail.ru>
2023-10-27 20:46:26 +0300

    initial code

--- src/main/java/com/collection/hw1/controller/EmployeeController.java
+++ src/main/java/com/collection/hw1/controller/EmployeeController.java
@@ -1,12 +1,36 @@
 package com.collection.hw1.controller;
 
+import com.collection.hw1.Employee;
 import com.collection.hw1.service.EmployeeService;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/employee")
 public class EmployeeController {
+    @ExceptionHandler(RuntimeException.class)
+    public String handleExceptoin(RuntimeException e) {
+        return e.getMessage();
+    }
     private final EmployeeService employeeService;
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
+    }
+    @GetMapping("/add")
+    public Employee add(@RequestParam String firstName, @RequestParam String lastName) {
+        return employeeService.add(firstName,lastName);
+    }
+    @GetMapping("/remove")
+    public Employee remove(@RequestParam String firstName, @RequestParam String lastName) {
+        return employeeService.remove(firstName,lastName);
+    }
+    @GetMapping("/get")
+    public Employee get(@RequestParam String firstName, @RequestParam String lastName) {
+        return employeeService.get(firstName,lastName);
+    }
+    @GetMapping
+    public List<Employee> getAll(){
+        return employeeService.getAll();
     }
 }
--- src/main/java/com/collection/hw1/service/EmployeeService.java
+++ src/main/java/com/collection/hw1/service/EmployeeService.java
@@ -2,6 +2,9 @@ package com.collection.hw1.service;
 
 
 import com.collection.hw1.Employee;
+import com.collection.hw1.exception.EmployeeAlreadyAddedException;
+import com.collection.hw1.exception.EmployeeNotFoundedException;
+import com.collection.hw1.exception.EmployeeStorageIsFullException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -9,14 +12,52 @@ import java.util.Arrays;
 import java.util.List;
 @Service
 public class EmployeeService {
-    private Employee[] employees = new Employee[10];
-    public Employee add (String firstName, String lastName) {
-        return null;
+    private static final int MAX_SIZE = 2;
+    private final List<Employee> employees = new ArrayList<>();
+
+    public Employee add(String firstName, String lastName) {
+
+        if (employees.size() > MAX_SIZE) {
+            throw new EmployeeStorageIsFullException("Массив сотрудников переполнен");
+        }
+
+        Employee newEmployee = new Employee(firstName, lastName);
+        if (employees.contains(newEmployee)) {
+            throw new EmployeeAlreadyAddedException("Такой сотрудник уже есть");
+        }
+
+        employees.add(newEmployee);
+        return newEmployee;
     }
-    public Employee remove (String firstName, String lastName) {
-        return null;
+
+    public Employee remove(String firstName, String lastName) {
+        Employee employeeForRemove = new Employee(firstName, lastName);
+
+
+        if (!employees.contains(employeeForRemove)) {
+            throw new EmployeeNotFoundedException("Невозможно удалить, сотрудника не существует");
+        }
+        employees.remove(employeeForRemove);
+        return employeeForRemove;
     }
-    public Employee get (String firstName, String lastName) {
-        return null;
+
+    public Employee get(String firstName, String lastName) {
+        Employee employeeForSearch = new Employee(firstName, lastName);
+
+        if (!employees.contains(employeeForSearch)) {
+            throw new EmployeeNotFoundedException("Невозможно найти, сотрудника не существует");
+        }
+
+            Employee result = null;
+            for (Employee employee : employees) {
+                if (employee.equals(employeeForSearch)) {
+                    return employee;
+                }
+            }
+            return result;
     }
-}
+        public List<Employee> getAll() {
+           return employees;
+        }
+    }
+
